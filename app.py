3959535c307d4d2f8bc65f4089895350491c1bd5
Valentin Smirnov <24082005vsv@gmail.com>
2024-10-22 15:38:47 +0300

    2commit

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask (__name__)
+app.config ['SQLALCHEMY DATABASE_URI'] = 'sqlite:///movies.db'
+db = SQLAlchemy(app)
 
+class Article(db.Model):
+    id = db.Column(db.Integer, primery_key = True)
+    title = db.Column(db.String(100), nullable = False)
+    intro = db.Column(db.String(300), nullable = False)
+    text = db.Column (db.Text, nullable = False)
+    date = db.Column (db.DateTime, default = datetime.utcnow)
+
+    def __repr__(self):
+        return '<Article %r>' % self.id
+    
 
 @app.route('/')
 @app.route('/home')
